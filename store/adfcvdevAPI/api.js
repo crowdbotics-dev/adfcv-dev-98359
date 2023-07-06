b429216faf47a9810e91be4856a756a21846aab5
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 12:58:58 +0000

    Update store.

---
+++ store/adfcvdevAPI/api.js
@@ -0,0 +1,89 @@
+import axios from "axios"
+const adfcvdevAPI = axios.create({
+  baseURL: "https://adfcv-dev-98359.botics.co",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_docs_schema_retrieve(payload) {
+  return adfcvdevAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
+}
+function api_v1_login_create(payload) {
+  return adfcvdevAPI.post(`/api/v1/login/`, payload.data)
+}
+function api_v1_signup_create(payload) {
+  return adfcvdevAPI.post(`/api/v1/signup/`, payload.data)
+}
+function api_v1_xccv_list(payload) {
+  return adfcvdevAPI.get(`/api/v1/xccv/`)
+}
+function api_v1_xccv_create(payload) {
+  return adfcvdevAPI.post(`/api/v1/xccv/`, payload.data)
+}
+function api_v1_xccv_retrieve(payload) {
+  return adfcvdevAPI.get(`/api/v1/xccv/${payload.id}/`)
+}
+function api_v1_xccv_update(payload) {
+  return adfcvdevAPI.put(`/api/v1/xccv/${payload.id}/`, payload.data)
+}
+function api_v1_xccv_partial_update(payload) {
+  return adfcvdevAPI.patch(`/api/v1/xccv/${payload.id}/`, payload.data)
+}
+function api_v1_xccv_destroy(payload) {
+  return adfcvdevAPI.delete(`/api/v1/xccv/${payload.id}/`)
+}
+function rest_auth_login_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/login/`, payload.data)
+}
+function rest_auth_logout_retrieve(payload) {
+  return adfcvdevAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/password/change/`, payload.data)
+}
+function rest_auth_password_reset_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/password/reset/`, payload.data)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+}
+function rest_auth_registration_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/registration/`, payload.data)
+}
+function rest_auth_registration_verify_email_create(payload) {
+  return adfcvdevAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+}
+function rest_auth_user_retrieve(payload) {
+  return adfcvdevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return adfcvdevAPI.put(`/rest-auth/user/`, payload.data)
+}
+function rest_auth_user_partial_update(payload) {
+  return adfcvdevAPI.patch(`/rest-auth/user/`, payload.data)
+}
+export const apiService = {
+  api_docs_schema_retrieve,
+  api_v1_login_create,
+  api_v1_signup_create,
+  api_v1_xccv_list,
+  api_v1_xccv_create,
+  api_v1_xccv_retrieve,
+  api_v1_xccv_update,
+  api_v1_xccv_partial_update,
+  api_v1_xccv_destroy,
+  rest_auth_login_create,
+  rest_auth_logout_retrieve,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
